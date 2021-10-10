aa960854f85e5713fc2aadcca1c8c0805b043a43
Tony <ygxan18@gmail.com>
2021-10-10 21:47:59 +0630

    .

--- netlify/functions/SpotifyAuth.js
+++ netlify/functions/SpotifyAuth.js
@@ -1,13 +1,12 @@
-const handler = () => {
+const handler = (payload) => {
   try {
+    const { baseUrl } = JSON.parse(payload.body)
     const authURL = new URL('https://accounts.spotify.com/authorize')
     authURL.searchParams.append('client_id', process.env.SPOTIFY_CLIENT_ID)
     authURL.searchParams.append('response_type', 'code')
-    authURL.searchParams.append(
-      'redirect_uri',
-      `https://yehtetaung.netlify.app/.netlify/functions/SpotifyCallback`
-    )
+    authURL.searchParams.append('redirect_uri', `${baseUrl}/.netlify/functions/SpotifyCallback`)
     authURL.searchParams.append('scope', 'user-read-playback-state')
+    console.log(baseUrl)
     return { statusCode: 200, body: authURL.href }
   } catch (error) {
     return {
